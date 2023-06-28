f3a36bdba2192a199ac55434f0075b76f76f053d
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2023-06-28 11:19:51 -0400

    Update create-cluster.sh

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -6,10 +6,10 @@ MIN_NODE_COUNT=$5
 
 FILE_PATH=infra/${NAME}-cluster.yaml
 
-cp crossplane/cluster-template-new.yaml $FILE_PATH
+cp crossplane/cluster-template.yaml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
-# yq --inplace ".spec.id = \"${NAME}\"" $FILE_PATH
-yq --inplace ".spec.compositionRef.name = \"${PROVIDER}\"" $FILE_PATH
-# yq --inplace ".spec.compositionSelector.matchLabels.cluster = \"${CLUSTER}\"" $FILE_PATH
+yq --inplace ".spec.id = \"${NAME}\"" $FILE_PATH
+yq --inplace ".spec.compositionSelector.matchLabels.provider = \"${PROVIDER}\"" $FILE_PATH
+yq --inplace ".spec.compositionSelector.matchLabels.cluster = \"${CLUSTER}\"" $FILE_PATH
 yq --inplace ".spec.parameters.nodeSize = \"${NODE_SIZE}\"" $FILE_PATH
 yq --inplace ".spec.parameters.minNodeCount = ${MIN_NODE_COUNT}" $FILE_PATH
