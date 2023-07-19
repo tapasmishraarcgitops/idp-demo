f66788ea96e41ce5b1a6d8dace3786f4aa7ff968
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2023-07-19 15:46:20 -0400

    Update create-pds.sh

--- scripts/create-pds.sh
+++ scripts/create-pds.sh
@@ -2,6 +2,10 @@ NAME=$1
 
 FILE_PATH=gcpconfig/namespaces/${NAME}/namespace.yaml
 
+cd gcpconfig/namespaces
+mkdir $(NAME)
+cd ..
+cd..
 cp anthos/pds-template.yaml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
 
